7c7d7f1fa0f065a76b91efd7f4a5388c3e22e442
Antoine Meausoone <antoine.meausoone@gmail.com>
2019-11-18 13:40:22 +0100

    feat(namespace): handle request on vault namespace

--- action.js
+++ action.js
@@ -5,17 +5,22 @@ const got = require('got');
 async function exportSecrets() {
     const vaultUrl = core.getInput('url', { required: true });
     const vaultToken = core.getInput('token', { required: true });
+    const vaultNamespace = core.getInput('namespace', { required: false });
 
     const secretsInput = core.getInput('secrets', { required: true });
     const secrets = parseSecretsInput(secretsInput);
 
     for (const secret of secrets) {
         const { secretPath, outputName, secretKey } = secret;
-        const result = await got(`${vaultUrl}/v1/secret/data/${secretPath}`, {
+        var headers = {
             headers: {
                 'X-Vault-Token': vaultToken
-            }
-        });
+            }};
+        if (vaultNamespace != null){
+            headers.headers["X-Vault-Namespace"] = vaultNamespace
+        }
+
+        const result = await got(`${vaultUrl}/v1/secret/data/${secretPath}`, headers);
 
         const parsedResponse = JSON.parse(result.body);
         const vaultKeyData = parsedResponse.data;
