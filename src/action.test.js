0f409d4023fcab6c346cc93f58f1bafc39829af0
Luis (LT) Carbonell <lt.carbonell@gmail.com>
2023-01-17 10:42:49 -0600

    Add Decoding to Secrets (#408)
    * Add decoding to secrets
    
    * remove index.js
    
    * Add test case, and other updates

--- src/action.test.js
+++ src/action.test.js
@@ -184,11 +184,30 @@ describe('exportSecrets', () => {
             .mockReturnValueOnce(doExport);
     }
 
+    function mockEncodeType(doEncode) {
+        when(core.getInput)
+            .calledWith('secretEncodingType', expect.anything())
+            .mockReturnValueOnce(doEncode);
+    }
+
     it('simple secret retrieval', async () => {
         mockInput('test key');
         mockVaultData({
             key: 1
         });
+
+        await exportSecrets();
+
+        expect(core.exportVariable).toBeCalledWith('KEY', '1');
+        expect(core.setOutput).toBeCalledWith('key', '1');
+    });
+
+    it('encoded secret retrieval', async () => {
+        mockInput('test key');
+        mockVaultData({
+            key: 'MQ=='
+        });
+        mockEncodeType('base64');
 
         await exportSecrets();
 
