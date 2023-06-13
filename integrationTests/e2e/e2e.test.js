62aa8bb4c43190676348ad2b146075f0421136db
Thy Ton <maithytonn@gmail.com>
2023-06-13 11:22:43 -0700

    Fix e2e CI failure from "Test that overwrite env var works in nested action (#453)"  (#467)
    Fix e2e CI failure VAULT-17057
    Co-authored-by: Leonardo Villela <leonardo.villela37@gmail.com>

--- integrationTests/e2e/e2e.test.js
+++ integrationTests/e2e/e2e.test.js
@@ -9,5 +9,6 @@ describe('e2e', () => {
         expect(process.env.OTHERALTSECRET).toBe("OTHERCUSTOMSECRET");
         expect(process.env.FOO).toBe("bar");
         expect(process.env.NAMED_CUBBYSECRET).toBe("zap");
+        expect(process.env.SUBSEQUENT_TEST_SECRET).toBe("SUBSEQUENT_TEST_SECRET");
     });
 });
