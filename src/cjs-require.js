3c8aedb297d551071df3e2b0f5064e010174f889
JM Faircloth <jmfaircloth@hashicorp.com>
2024-03-11 13:51:11 -0500

    add doc link comment

--- src/cjs-require.js
+++ src/cjs-require.js
@@ -1,3 +1,5 @@
+// This allows us to use `require` in our ECMAScript module
+// https://nodejs.org/api/module.html#modulecreaterequirefilename
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 
